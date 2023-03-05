30fd0d6fd72ec4531ceabcfe057ca10e2b74bb05
coderufat1996 <rufat.karimli96@mail.ru>
2023-03-05 15:20:07 +0400

    'yukle'

---
+++ ConsoleApp1Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 9;
+        }
+    }
+}
